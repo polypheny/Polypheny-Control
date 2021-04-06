6d53c7fe38bb5e1d3aa6f0cd326d4fab4a94f129
Vishal Dalwadi <dalwadivishal26@gmail.com>
2021-04-06 23:11:36 +0530

    Authentication for REST API.

---
+++ src/main/resources/static/login.js
@@ -0,0 +1,23 @@
+function login() {
+    let name = $('#name').val();
+    let password = $('#password').val();
+    let authHeader = "Basic " + btoa(name + ":" + password);
+    console.log(name);
+    console.log(password);
+    console.log(authHeader);
+    $.ajax({
+        url        : document.location.protocol + '//' + document.location.host + '/',
+        type       : 'GET',
+        headers    : {
+            "Authorization": authHeader
+        },
+        success    : function(data) {
+            document.location = document.location.protocol + '//' + document.location.host + '/'
+        },
+        error      : function(d, data) {
+            console.log(data);
+            $('#invalid').css('display', 'block');
+        },
+    });
+    return false;
+}
